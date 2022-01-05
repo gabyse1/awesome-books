da6b613b30213ae1e1c29ab971883789feb8b335
iamgaby <gaby.es.iaa@gmail.com>
2022-01-05 18:08:02 -0500

    Fix styles

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,39 +1,37 @@
-const booklist = document.querySelector(".booklist-container");
-const addnew = document.querySelector(".addnew-container");
-const contact = document.querySelector(".contact-container");
+const booklist = document.querySelector('.booklist-container');
+const addnew = document.querySelector('.addnew-container');
+const contact = document.querySelector('.contact-container');
 
-const listLink = document.querySelector("#list-link");
-const addnewLink = document.querySelector("#addnew-link");
-const contactLink = document.querySelector("#contact-link");
+const listLink = document.querySelector('#list-link');
+const addnewLink = document.querySelector('#addnew-link');
+const contactLink = document.querySelector('#contact-link');
 
-listLink.addEventListener("click", (e) => {
+listLink.addEventListener('click', (e) => {
   e.preventDefault();
-  booklist.classList.remove("d-none");
-  addnew.classList.add("d-none");
-  contact.classList.add("d-none");
-  listLink.classList.add("active");
-  addnewLink.classList.remove("active");
-  contactLink.classList.remove("active");
-
+  booklist.classList.remove('d-none');
+  addnew.classList.add('d-none');
+  contact.classList.add('d-none');
+  listLink.classList.add('active');
+  addnewLink.classList.remove('active');
+  contactLink.classList.remove('active');
 });
 
-addnewLink.addEventListener("click", (e) => {
+addnewLink.addEventListener('click', (e) => {
   e.preventDefault();
-  booklist.classList.add("d-none");
-  addnew.classList.remove("d-none");
-  contact.classList.add("d-none");
-  listLink.classList.remove("active");
-  addnewLink.classList.add("active");
-  contactLink.classList.remove("active");
-
+  booklist.classList.add('d-none');
+  addnew.classList.remove('d-none');
+  contact.classList.add('d-none');
+  listLink.classList.remove('active');
+  addnewLink.classList.add('active');
+  contactLink.classList.remove('active');
 });
 
-contactLink.addEventListener("click", (e) => {
+contactLink.addEventListener('click', (e) => {
   e.preventDefault();
-  booklist.classList.add("d-none");
-  addnew.classList.add("d-none");
-  contact.classList.remove("d-none");
-  listLink.classList.remove("active");
-  addnewLink.classList.remove("active");
-  contactLink.classList.add("active");
+  booklist.classList.add('d-none');
+  addnew.classList.add('d-none');
+  contact.classList.remove('d-none');
+  listLink.classList.remove('active');
+  addnewLink.classList.remove('active');
+  contactLink.classList.add('active');
 });
